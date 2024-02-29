853e96b9b6e74ed9d693acf0cd6f06ffdcf7c152
mustafanafia <55895079+mustafanafia@users.noreply.github.com>
2024-02-29 15:50:34 -0500

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+
+        Set<Integer> set = new HashSet();
+        int numberOfPairs=0;
+
+        if(ar.size() == numberOfPairs){return numberOfPairs;}
+
+        for(int i = 0; i < ar.size(); i++){
+            if(!set.contains(ar.get(i))){
+                set.add(ar.get(i));
+            }
+            else {
+                numberOfPairs++;
+                set.remove(ar.get(i));
+            }
+        }
+        return numberOfPairs++;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> ar = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arTemp[i]);
+            ar.add(arItem);
+        }
+
+        int result = Result.sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
